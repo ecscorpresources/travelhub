07bd91860e23e457dcd64d3d1d999e67b0518692
rasheedshina22 <rasheedshinaopeyemi@gmail.com>
2020-02-05 09:00:15 +0100

    added spinner component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
+import { ThemeProvider } from "@chakra-ui/core";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "../includes/Navbar";
-import Homepage from "../pages/Homepage/Homepage";
-import Footer from "../includes/Footer";
-import { ThemeProvider } from "@chakra-ui/core";
-import ResortHotels from "../pages/ResortHotels/ResortHotels";
-import ToursGuide from "../pages/ToursGuide/ToursGuide";
-import CarRental from "../pages/CarRentals/CarRental";
-import MainCruise from "../pages/Cruise/MainCruise";
+import Spinner from "../includes/Spinner";
+const Homepage = lazy(() => import("../pages/Homepage/Homepage"));
+const Footer = lazy(() => import("../includes/Footer"));
+const ResortHotels = React.lazy(() =>
+  import("../pages/ResortHotels/ResortHotels")
+);
+const ToursGuide = lazy(() => import("../pages/ToursGuide/ToursGuide"));
+const CarRental = lazy(() => import("../pages/CarRentals/CarRental"));
+const MainCruise = lazy(() => import("../pages/Cruise/MainCruise"));
 
 function App() {
   return (
     <>
       <Router>
         <ThemeProvider>
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route path="/resorts_hotels" component={ResortHotels} />
-            <Route path="/tours_guides" component={ToursGuide} />
-            <Route path="/car_rental" component={CarRental} />
-            <Route path="/cruise" component={MainCruise} />
-          </Switch>
-          <Footer />
+          <Suspense fallback={<Spinner />}>
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Homepage} />
+              <Route path="/resorts_hotels" component={ResortHotels} />
+              <Route path="/tours_guides" component={ToursGuide} />
+              <Route path="/car_rental" component={CarRental} />
+              <Route path="/cruise" component={MainCruise} />
+            </Switch>
+            <Footer />
+          </Suspense>
         </ThemeProvider>
       </Router>
     </>
